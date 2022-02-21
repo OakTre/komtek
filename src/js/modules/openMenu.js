b55e1e300eb25a1566ff64a11efa2f5054cf29a6
Nail <nailgalimov8@gmail.com>
2022-02-21 11:03:03 +0300

    upd

--- src/js/modules/openMenu.js
+++ src/js/modules/openMenu.js
@@ -3,9 +3,10 @@ import gsap from "gsap";
 export default () => {
 	gsap.set(".js-menu", {y: "-100%"});
 	gsap.set(".menu__bottom-block", {y: "2rem", opacity: 0});
+	gsap.set(".menu__md-block", {y: "1rem", opacity: 0});
 	gsap.set(".menu__top-block", {y: "-2rem", opacity: 0});
 
-	const openMenuBtn = document.querySelector(".js-open-menu");
+	const openMenuBtns = document.querySelectorAll(".js-open-menu");
 
 	function isReversed(animation) {
 		animation.reversed() ? animation.play() : animation.reverse();
@@ -17,13 +18,15 @@ export default () => {
 	});
 
 	timeline
-		.to(".js-menu", {y: 0, duration: 1, ease: "power4.out"})
-		.to(".menu__bottom-block", {y: 0, opacity: 1, duration: 1, ease: "power4.out"}, "-=0.5")
-		.to(".menu__top-block", {y: 0, opacity: 1, duration: 1, ease: "power4.out"}, "-=1")
+		.to(".js-menu", {y: 0, duration: 0.8, ease: "power4.out"})
+		.to(".menu__bottom-block", {y: 0, opacity: 1, duration: 0.8, ease: "power4.out"}, "-=0.4")
+		.to(".menu__md-block", {y: 0, opacity: 1, duration: 0.8, ease: "power4.out"}, "-=0.4")
+		.to(".menu__top-block", {y: 0, opacity: 1, duration: 0.8, ease: "power4.out"}, "-=0.8")
 
-	openMenuBtn.addEventListener("click", ()=>{
-		isReversed(timeline);
-		openMenuBtn.classList.toggle("is-active");
-
-	});
+	openMenuBtns.forEach((btn)=>{
+		btn.addEventListener("click", ()=>{
+			isReversed(timeline);
+			openMenuBtns.forEach((btn)=>{btn.classList.toggle("is-active");})
+		});
+	})
 };
