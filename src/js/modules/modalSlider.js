2d8f91b1577ca3fc45dde19aacd97979a23a382c
OakTre <nailgalimov8@gmail.com>
2022-05-20 17:05:45 +0300

    upd

--- src/js/modules/modalSlider.js
+++ src/js/modules/modalSlider.js
@@ -1,19 +1,17 @@
 import { Navigation, Swiper } from 'swiper';
 
 export default () => {
-	const modalEvent = document.querySelector(".modal-event__slider");
+	const modalEvent = Array.from(document.querySelectorAll(".modal-event__slider"));
 	if (!modalEvent) return;
 
-	let modalEventSlider = new Swiper(modalEvent, {
-		slidesPerView: 1.3,
-        spaceBetween: 20,
-		navigation: {
-			nextEl: '.js-modal-slider-nav-next',
-			prevEl: '.js-modal-slider-nav-prev',
-		},
+	modalEvent.forEach( (slider) => {
+		let modalEventSlider = new Swiper(slider, {
+			slidesPerView: 1.3,
+			spaceBetween: 20,
+			navigation: {
+				nextEl: '.js-modal-slider-nav-next',
+				prevEl: '.js-modal-slider-nav-prev',
+			},
+		});
 	});
-
-	window.modalSliderUpdate = () => {
-		modalEventSlider.update();
-	};
 };
